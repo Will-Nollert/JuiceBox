fb6dd7eb6eabb3db5440eb94632c15ab2d8b781f
William Nollert <will.nollert@gmail.com>
2022-01-04 11:46:37 -0600

    added getAllUsers helper function and formating

--- db/index.js
+++ db/index.js
@@ -1,6 +1,21 @@
 const { Client } = require('pg'); // imports the pg module
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
+
+
+//start of helper functions 
+
+async function getAllUsuers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+    FROM users;
+    `);
+
+    return rows;
+}
+
+//exports here 
 module.exports = {
   client,
+  getAllUsuers,
 }
