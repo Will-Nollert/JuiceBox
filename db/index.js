35b890e5bd70dcacd87756c605e8f5a9d44a0f8d
William Nollert <will.nollert@gmail.com>
2022-01-12 21:01:52 -0600

    finals days work

--- db/index.js
+++ db/index.js
@@ -61,6 +61,20 @@ async function updateUser(id, fields = {}) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [username]);
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 //-----start of Post helper functions
 
 async function createPost({
@@ -334,4 +348,5 @@ module.exports = {
   addTagsToPost,
   getPostsByTagName,
   getAllTags,
+  getUserByUsername,
 };
